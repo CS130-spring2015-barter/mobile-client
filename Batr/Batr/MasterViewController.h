1d95abcd77704db03376511c3827a23bcdcb2056
Mickey Sweatt <sweattm92@gmail.com>
2015-04-12 20:40:33 -0700

    Added intial skelton for iOS app

---
+++ Batr/Batr/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Batr
+//
+//  Created by admin on 4/12/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
