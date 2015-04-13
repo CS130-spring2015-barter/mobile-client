60bc961d99a3f49b4d53b503359a607ae7257347
Mickey Sweatt <sweattm92@gmail.com>
2015-04-12 23:24:17 -0700

    Added skelton for VCs

---
+++ Bartr/Bartr/ItemsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsTableViewController.h
+//  Bartr
+//
+//  Created by admin on 4/12/15.
+//  Copyright (c) 2015 Bartr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemsTableViewController : UITableViewController
+
+@end
---
+++ Bartr/Bartr/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Bartr
+//
+//  Created by admin on 4/12/15.
+//  Copyright (c) 2015 Bartr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
