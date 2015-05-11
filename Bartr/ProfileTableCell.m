d05289710be987eda1a9d2b1035ae0cc0f1ab077
Matt Nguyen <mattnguyen81@yahoo.com>
2015-05-10 22:33:21 -0700

    Working table with label fields

---
+++ Bartr/ProfileTableCell.m
@@ -0,0 +1,13 @@
+//
+//  ProfileTableCell.m
+//  Bartr
+//
+//  Created by Andrea Kuan on 5/10/15.
+//  Copyright (c) 2015 Bartr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ProfileTableCell.h"
+
+@implementation ProfileTableCell
+@end
