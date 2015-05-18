9f8e84ade518b65449ec1335d174972d8bb915ba
Mickey Sweatt <sweattm92@gmail.com>
2015-05-18 12:08:21 -0700

    Added input validation to email for login

--- Bartr/LoginViewController.m
+++ Bartr/LoginViewController.m
@@ -32,7 +32,16 @@
         [self alertStatus:@"Please enter Email and Password" :@"Sign in Failed!" :0];
     }
     else {
-        user = [BrtrDataSource getUserForEmail:email password:password];
+        // Validate emails
+        NSError* error;
+        NSRegularExpression *regex = [[NSRegularExpression alloc ]initWithPattern:@"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$" options:NSRegularExpressionCaseInsensitive error:&error];
+        NSTextCheckingResult *result = [regex firstMatchInString:email options:0 range:NSMakeRange(0, [email length])];
+        if (nil == result) {
+            [self alertStatus:@"Invalid email" :@"Sign in Failed" :0];
+        }
+        else {
+            user = [BrtrDataSource getUserForEmail:email password:password];
+        }
         //  user = [BrtrDataSource getUserForEmail:@"foo@bar.com"];
     }
     if (nil != user) {
