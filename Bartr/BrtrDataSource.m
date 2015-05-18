697b8f29e19f6d04a3736094fb09ced93e45be71
Mickey Sweatt <sweattm92@gmail.com>
2015-05-17 23:08:10 -0700

    Fixed semantic conflict in keychain code, and added mock for getting cards

--- Bartr/BrtrDataSource.m
+++ Bartr/BrtrDataSource.m
@@ -383,9 +383,25 @@
     [ap startLocationManager];
     
     CLLocation *location = [ap getGPSData];
-    NSString *routeAsString = [NSString stringWithFormat:@"item/geo"];
-
     
+    
+    NSURLRequest *request = [BrtrDataSource getRequestWith:@"item/geo" andQuery:[NSString stringWithFormat:@"lat=%f&long=%f",location.coordinate.latitude, location.coordinate.latitude]];
+    NSError * error;
+    NSHTTPURLResponse *response;
+    NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+    NSLog(@"Response code: %ld", (long)[response statusCode]);
+    NSDictionary *jsonData;
+    if ([response statusCode] >= 200 && [response statusCode] < 300)
+    {
+
+        
+        error = nil;
+        jsonData = [NSJSONSerialization
+                    JSONObjectWithData:urlData
+                    options:NSJSONReadingMutableContainers
+                    error:&error];
+        NSLog(@"Data ===>  %@", jsonData);
+    }
     NSManagedObjectContext *context = [[JCDCoreData sharedInstance] defaultContext];
     NSArray *matches =  [context fetchObjectsWithEntityName:@"BrtrCardItem" sortedBy:nil withPredicate:[NSPredicate predicateWithFormat:@"user.email = %@", user.email]];
     if (!matches || 0 == [matches count]) {
