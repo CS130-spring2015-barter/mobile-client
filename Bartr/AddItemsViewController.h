0c807647f0563ced8613efc2ae342245e1a23db0
Mickey Sweatt <sweattm92@gmail.com>
2015-05-19 19:20:20 -0700

    Make AddItemViewController cells editable

---
+++ Bartr/AddItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddItemsViewController.h
+//  Bartr
+//
+//  Created by admin on 5/19/15.
+//  Copyright (c) 2015 Bartr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemsViewController : UIViewController<UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate>
+
+@end
