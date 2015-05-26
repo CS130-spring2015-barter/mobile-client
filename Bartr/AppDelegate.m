15930a9e578f0737dfaeccecd634264ee0f56a60
synthling <synthialing@gmail.com>
2015-05-26 16:15:38 -0700

    Remove ConversationListViewController

--- Bartr/AppDelegate.m
+++ Bartr/AppDelegate.m
@@ -11,7 +11,6 @@
 #import "BrtrDataSource.h"
 #import <Security/Security.h>
 #import "KeychainItemWrapper.h"
-#import "ConversationListViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
 @interface AppDelegate ()
